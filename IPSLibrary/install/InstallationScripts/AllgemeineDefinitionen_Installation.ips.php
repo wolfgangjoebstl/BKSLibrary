fcba0dcdf4e6f7890f099753fa6a22552168375a
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2014-09-18 23:13:46 +0200

    Allgemeine Def als IPSLIb Modul

---
+++ IPSLibrary/install/InstallationScripts/AllgemeineDefinitionen_Installation.ips.php
@@ -0,0 +1,43 @@
+<?
+
+	/**@defgroup ipstwilight IPSTwilight
+	 * @ingroup modules_weather
+	 * @{
+	 *
+	 * Script zur Ansteuerung der Giessanlage in BKS
+	 *
+	 *
+	 * @file          Gartensteuerung_Installation.ips.php
+	 * @author        Wolfgang Joebstl
+	 * @version
+	 *  Version 2.50.44, 07.08.2014<br/>
+	 **/
+	 
+	Include_once(IPS_GetKernelDir()."scripts\IPSLibrary/AllgemeineDefinitionen.inc.php");
+	 
+	
+	$repository = 'https://raw.githubusercontent.com//wolfgangjoebstl/BKSLibrary/master/';
+	if (!isset($moduleManager)) {
+		IPSUtils_Include ('IPSModuleManager.class.php', 'IPSLibrary::install::IPSModuleManager');
+
+		echo 'ModuleManager Variable not set --> Create "default" ModuleManager';
+		$moduleManager = new IPSModuleManager('Startpage',$repository);
+	}
+
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPS','2.50');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSModuleManager','2.50.3');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSLogger','2.50.2');
+
+	echo "\nKernelversion : ".IPS_GetKernelVersion();
+	$ergebnis=$moduleManager->VersionHandler()->GetScriptVersion();
+	echo "\nIPS Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetModuleState();
+	echo " ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('IPSModuleManager');
+	echo "\nIPSModulManager Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('Startpage');
+	echo "\nRemoteReadWrite Version : ".$ergebnis;
+	
+
+
+?>
