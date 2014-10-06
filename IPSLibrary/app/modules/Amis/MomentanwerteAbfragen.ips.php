991df03aef38d11c66a08c713763be076a88954d
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2014-10-06 22:42:20 +0200

    LBG Files Anlegfen

---
+++ IPSLibrary/app/modules/Amis/MomentanwerteAbfragen.ips.php
@@ -0,0 +1,26 @@
+<?
+
+/*
+	 * @defgroup 
+	 * @ingroup
+	 * @{
+	 *
+	 * Script zur 
+	 *
+	 *
+	 * @file      
+	 * @author        Wolfgang Joebstl
+	 * @version
+	 *  Version 2.50.52, 07.08.2014<br/>
+*/
+
+Include_once(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
+
+/******************************************************
+
+				INIT
+				
+*************************************************************/
+
+	
+?>
