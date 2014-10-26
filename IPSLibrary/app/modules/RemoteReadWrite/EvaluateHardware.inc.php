993c50e7bfcfbc06ed398af85642435191a660e7
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2014-10-26 16:01:06 +0100

    BKS EvaluateHW Default

--- IPSLibrary/app/modules/RemoteReadWrite/EvaluateHardware.inc.php
+++ IPSLibrary/app/modules/RemoteReadWrite/EvaluateHardware.inc.php
@@ -2,5 +2,12 @@
 
 /* Evaluate Hardware.inc wird automatisch erstellt */
 
+function FHTList() { return array(
+);}
+function FS20List() { return array(
+);}
+function HomematicList() { return array(
+);}
+
 
 ?>
