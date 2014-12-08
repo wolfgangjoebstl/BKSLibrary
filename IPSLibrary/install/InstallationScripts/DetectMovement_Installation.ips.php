4afd61fa4f4ec0d6c7815d7dde4f5c3c00bac6de
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2014-12-08 11:37:25 +0100

    weiter1

---
+++ IPSLibrary/install/InstallationScripts/DetectMovement_Installation.ips.php
@@ -0,0 +1,107 @@
+<?
+
+	/**@defgroup ipstwilight IPSTwilight
+	 * @ingroup modules_weather
+	 * @{
+	 *
+	 * Script um Herauszufinden ob wer zu Hause ist
+	 *
+	 *
+	 * @file          DetectMovement_Installation.ips.php
+	 * @author        Wolfgang Joebstl
+	 * @version
+	 *  Version 2.50.1, 07.12.2014<br/>
+	 **/
+
+	Include_once(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
+
+	//$repository = 'https://10.0.1.6/user/repository/';
+	$repository = 'https://raw.githubusercontent.com//wolfgangjoebstl/BKSLibrary/master/';
+	if (!isset($moduleManager)) {
+		IPSUtils_Include ('IPSModuleManager.class.php', 'IPSLibrary::install::IPSModuleManager');
+
+		echo 'ModuleManager Variable not set --> Create "default" ModuleManager';
+		$moduleManager = new IPSModuleManager('RemoteReadWrite',$repository);
+	}
+
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPS','2.50');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSModuleManager','2.50.3');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSLogger','2.50.2');
+
+	echo "\nKernelversion : ".IPS_GetKernelVersion();
+	$ergebnis=$moduleManager->VersionHandler()->GetScriptVersion();
+	echo "\nIPS Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetModuleState();
+	echo " ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('IPSModuleManager');
+	echo "\nIPSModulManager Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('DetectMovement');
+	echo "\nRemoteAccess Version : ".$ergebnis;
+
+ 	$installedModules = $moduleManager->GetInstalledModules();
+	$inst_modules="\nInstallierte Module:\n";
+	foreach ($installedModules as $name=>$modules)
+		{
+		$inst_modules.=str_pad($name,20)." ".$modules."\n";
+		}
+	echo $inst_modules;
+	
+	IPSUtils_Include ("IPSInstaller.inc.php",                       "IPSLibrary::install::IPSInstaller");
+	IPSUtils_Include ("IPSModuleManagerGUI.inc.php",                "IPSLibrary::app::modules::IPSModuleManagerGUI");
+	IPSUtils_Include ("IPSModuleManagerGUI_Constants.inc.php",      "IPSLibrary::app::modules::IPSModuleManagerGUI");
+
+	echo "\nWF10 ";
+	$WFC10_Enabled        = $moduleManager->GetConfigValue('Enabled', 'WFC10');
+	$WFC10_Path        	 = $moduleManager->GetConfigValue('Path', 'WFC10');
+
+	echo "WF10User ";
+	$WFC10User_Enabled    = $moduleManager->GetConfigValue('Enabled', 'WFC10User');
+	$WFC10User_Path        	 = $moduleManager->GetConfigValue('Path', 'WFC10User');
+
+	echo "Mobile ";
+	$Mobile_Enabled        = $moduleManager->GetConfigValue('Enabled', 'Mobile');
+	$Mobile_Path        	 = $moduleManager->GetConfigValue('Path', 'Mobile');
+	
+	echo "Retro \n";
+	$Retro_Enabled        = $moduleManager->GetConfigValue('Enabled', 'Retro');
+	$Retro_Path        	 = $moduleManager->GetConfigValue('Path', 'Retro');
+	echo "Test";
+	
+	$CategoryIdData     = $moduleManager->GetModuleCategoryID('data');
+	$CategoryIdApp      = $moduleManager->GetModuleCategoryID('app');
+	
+
+	// ----------------------------------------------------------------------------------------------------------------------------
+	// WebFront Installation
+	// ----------------------------------------------------------------------------------------------------------------------------
+	if ($WFC10_Enabled)
+		{
+		echo "\nWebportal Administrator installieren auf ".$WFC10_Path.": \n";
+		$categoryId_WebFront         = CreateCategoryPath($WFC10_Path);
+		}
+		
+	if ($WFC10User_Enabled)
+		{
+		echo "\nWebportal User installieren auf ".$WFC10User_Path.": \n";
+		$categoryId_WebFront         = CreateCategoryPath($WFC10User_Path);
+
+		}
+
+	if ($Mobile_Enabled)
+		{
+		echo "\nWebportal Mobile installieren auf ".$Mobile_Path.": \n";
+		$categoryId_WebFront         = CreateCategoryPath($Mobile_Path);
+
+		}
+
+	if ($Retro_Enabled)
+		{
+		echo "\nWebportal Retro installieren auf ".$Retro_Path.": \n";
+		createPortal($Retro_Path);
+		}
+
+
+
+
+
+?>
