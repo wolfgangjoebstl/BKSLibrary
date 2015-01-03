7ead64531750d6ec7875d37c4255a114ae21eb8b
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-01-03 23:17:07 +0100

    LBG CC new class for motion

---
+++ IPSLibrary/app/core/IPSComponent/IPSComponentSensor/IPSComponentSensor_Motion.class.php
@@ -0,0 +1,80 @@
+<?
+
+   /**
+    * @class IPSComponentSensor_Temperatur
+    *
+    * Definiert ein IPSComponentSensor_Temperatur Object, das ein IPSComponentSensor Object für einen Sensor implementiert.
+    *
+    * @author Wolfgang Jöbstl
+    * @version
+    *   Version 2.50.1, 09.06.2012<br/>
+    */
+
+	IPSUtils_Include ('IPSComponentSensor.class.php', 'IPSLibrary::app::core::IPSComponent::IPSComponentSensor');
+	IPSUtils_Include ("RemoteAccess_Configuration.inc.php","IPSLibrary::config::modules::RemoteAccess");
+			
+	class IPSComponentSensor_Temperatur extends IPSComponentSensor {
+
+
+		private $tempObject;
+		private $RemoteOID;
+		private $tempValue;
+
+		/**
+		 * @public
+		 *
+		 * Initialisierung eines IPSModuleSensor_IPStemp Objektes
+		 *
+		 * @param string $tempObject Licht Object/Name (Leuchte, Gruppe, Programm, ...)
+		 * @param integer $RemoteOID OID die gesetzt werden soll
+		 * @param string $tempValue Wert für Beleuchtungs Änderung
+		 */
+		public function __construct($var1, $lightObject=null, $lightValue=null) {
+			$this->tempObject   = $lightObject;
+			$this->RemoteOID    = $var1;
+			$this->tempValue    = $lightValue;
+			$this->remServer    = RemoteAccess_GetConfiguration();
+		}
+	
+		/**
+		 * @public
+		 *
+		 * Function um Events zu behandeln, diese Funktion wird vom IPSMessageHandler aufgerufen, um ein aufgetretenes Event 
+		 * an das entsprechende Module zu leiten.
+		 *
+		 * @param integer $variable ID der auslösenden Variable
+		 * @param string $value Wert der Variable
+		 * @param IPSModuleSensor $module Module Object an das das aufgetretene Event weitergeleitet werden soll
+		 */
+		public function HandleEvent($variable, $value, IPSModuleSensor $module){
+			echo "Temperatur Message Handler für VariableID : ".$variable." mit Wert : ".$value." \n";
+			//print_r($this);
+			//print_r($module);
+			//echo "-----Hier jetzt alles programmieren was bei Veränderung passieren soll:\n";
+			foreach ($this->remServer as $Server)
+				{
+				echo "Server : ".$Server."\n";
+				$rpc = new JSONRPC($Server);
+				echo "Remote OID: ".$this->RemoteOID."\n";
+				$roid=(integer)$this->RemoteOID;
+				$rpc->SetValue($roid, $value);
+				}
+		}
+
+		/**
+		 * @public
+		 *
+		 * Funktion liefert String IPSComponent Constructor String.
+		 * String kann dazu benützt werden, das Object mit der IPSComponent::CreateObjectByParams
+		 * wieder neu zu erzeugen.
+		 *
+		 * @return string Parameter String des IPSComponent Object
+		 */
+		public function GetComponentParams() {
+			return get_class($this);
+		}
+
+	}
+
+	/** @}*/
+?>
