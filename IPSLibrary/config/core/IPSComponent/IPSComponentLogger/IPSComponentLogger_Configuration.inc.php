8d5aabbcc7fe27c6223c3565852ac08961746e9a
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-01-04 13:36:23 +0100

    LBG CC cfg file

---
+++ IPSLibrary/config/core/IPSComponent/IPSComponentLogger/IPSComponentLogger_Configuration.inc.php
@@ -0,0 +1,44 @@
+<?
+	/*
+	 * This file is part of the IPSLibrary.
+	 *
+	 * The IPSLibrary is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published
+	 * by the Free Software Foundation, either version 3 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * The IPSLibrary is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with the IPSLibrary. If not, see http://www.gnu.org/licenses/gpl.txt.
+	 */    
+
+
+	/**@defgroup IPSComponentLogger
+	 * @ingroup IPSComponentLogger
+	 * @{
+	 *
+	 * Konfigurations File für IPSComponentLogger
+	 *
+	 * @file          IPSComponentLogger_Configuration.inc.php
+	 * @author        Wolfgang Joebstl
+	 * @version
+	 *  Version 2.50.1, 13.02.2012<br/>
+	 *
+	 */
+
+	function get_IPSComponentLoggerConfig()
+		{
+		return array(
+			"TemperatureLog"		=> "C:/Users/wolfg_000/Documents/iMacros/Macros/", 	/* Verzeichnis von iMacro */
+			"HumidityLog" 			=> "C:/Users/wolfg_000/Documents/iMacros/Downloads/", 	/* Verzeichnis von iMacro */
+			);
+		}
+
+	 
+
+	/** @}*/
+?>
