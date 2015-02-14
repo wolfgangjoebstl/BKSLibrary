2ec5d3dfe5173e9e998773297792048f222ad0bf
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-02-14 17:13:30 +0100

    BKS new Autosteuerung

---
+++ IPSLibrary/install/InstallationScripts/Autosteuerung_Installation.ips.php
@@ -0,0 +1,53 @@
+<?
+
+	/**@defgroup Stromheizung
+	 *
+	 * Script um elektrische Heizung nachzusteuern
+	 *
+	 *
+	 * @file          Stromheizung_Installation.ips.php
+	 * @author        Wolfgang Joebstl
+	 * @version
+	 *  Version 2.50.1, 07.12.2014<br/>
+	 **/
+
+	Include_once(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
+
+	$repository = 'https://raw.githubusercontent.com//wolfgangjoebstl/BKSLibrary/master/';
+	if (!isset($moduleManager)) {
+		IPSUtils_Include ('IPSModuleManager.class.php', 'IPSLibrary::install::IPSModuleManager');
+
+		echo 'ModuleManager Variable not set --> Create "default" ModuleManager';
+		$moduleManager = new IPSModuleManager('Stromheizung',$repository);
+	}
+
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPS','2.50');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSModuleManager','2.50.3');
+	$moduleManager->VersionHandler()->CheckModuleVersion('IPSLogger','2.50.2');
+
+	echo "\nKernelversion : ".IPS_GetKernelVersion();
+	$ergebnis=$moduleManager->VersionHandler()->GetScriptVersion();
+	echo "\nIPS Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetModuleState();
+	echo " ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('IPSModuleManager');
+	echo "\nIPSModulManager Version : ".$ergebnis;
+	$ergebnis=$moduleManager->VersionHandler()->GetVersion('Stromheizung');
+	echo "\nStromheizung Version : ".$ergebnis;
+
+ 	$installedModules = $moduleManager->GetInstalledModules();
+	$inst_modules="\nInstallierte Module:\n";
+	foreach ($installedModules as $name=>$modules)
+		{
+		$inst_modules.=str_pad($name,20)." ".$modules."\n";
+		}
+	echo $inst_modules;
+	
+	IPSUtils_Include ("IPSInstaller.inc.php",                       "IPSLibrary::install::IPSInstaller");
+	IPSUtils_Include ("IPSModuleManagerGUI.inc.php",                "IPSLibrary::app::modules::IPSModuleManagerGUI");
+	IPSUtils_Include ("IPSModuleManagerGUI_Constants.inc.php",      "IPSLibrary::app::modules::IPSModuleManagerGUI");
+
+	$CategoryIdData     = $moduleManager->GetModuleCategoryID('data');
+	$CategoryIdApp      = $moduleManager->GetModuleCategoryID('app');
+
+?>
