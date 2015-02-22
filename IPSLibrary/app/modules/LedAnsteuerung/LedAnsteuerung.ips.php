9c03e9da525ca61ae6bb1a914dffa86ecb448073
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-02-22 22:26:27 +0100

    LBG LA CC upd

--- IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung.ips.php
+++ IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung.ips.php
@@ -13,83 +13,76 @@
  //
 
 
-$LW12_LibraryId = 14824 /*[Program\Debug\RGB-LED\LW12-Library]*/;
-
-
- // Ab hier kommt der Installations-Code. Keine Änderungen in diesem Bereich vornehmen!!!
-
-$parentId = IPS_GetParent($_IPS['SELF']);
+$LW12_LibraryId  = IPSUtil_ObjectIDByPath('Program.IPSLibrary.app.modules.LedAnsteuerung.LedAnsteuerung_Library');
+$parentId = IPSUtil_ObjectIDByPath('Program.IPSLibrary.data.modules.LedAnsteuerung');
 
  // Dummy Instanz anlegen
 
-$modulID = @IPS_GetInstanceIDByName("LW12LEDController", $parentID);
-echo "ModulID :".$parentId." ".$modulID."\n";
+$name="LW12_Arbeitszimmer";
+$modulId = @IPS_GetInstanceIDByName($name, $parentId);
 
-
-break;
-
-if(!IPS_InstanceExists($modulID))
+if(!IPS_InstanceExists($modulId))
    {
 	$modulId = IPS_CreateInstance("{485D0419-BE97-4548-AA9C-C083EB82E61E}");
 
 	IPS_SetParent($modulId, $parentId);
-	IPS_SetName($modulId, "LW12LEDController");
+	IPS_SetName($modulId, $name);
+	IPS_ApplyChanges($modulId);
 
+	 // Variabeln anlegen
+	 // =================
 
- // Variabeln anlegen
- // =================
+	 //!IP
+	$var = IPS_CreateVariable(3);
+	IPS_SetParent($var, $modulId);
+	IPS_SetName($var, "!IP");
+	SetValue($var, "1.2.3.4");
+	IPS_SetHidden($var, true);
 
- //!IP
-$var = IPS_CreateVariable(3);
-IPS_SetParent($var, $modulId);
-IPS_SetName($var, "!IP");
-SetValue($var, "1.2.3.4");
-IPS_SetHidden($var, true);
+	 //!LW12_Library
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $modulId);
+	IPS_SetName($var, "!LW12_Library");
+	SetValue($var, $LW12_LibraryId);
+	IPS_SetHidden($var, true);
 
- //!LW12_Library
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $modulId);
-IPS_SetName($var, "!LW12_Library");
-SetValue($var, $LW12_LibraryId);
-IPS_SetHidden($var, true);
+	 //!TCP-Port
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $modulId);
+	IPS_SetName($var, "!TCP-Port");
+	SetValue($var, 5577);
+	IPS_SetHidden($var, true);
 
- //!TCP-Port
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $modulId);
-IPS_SetName($var, "!TCP-Port");
-SetValue($var, 5577);
-IPS_SetHidden($var, true);
-
- //Licht
-$var = IPS_CreateVariable(0);
-IPS_SetParent($var, $modulId);
-IPS_SetPosition($var, 10);
-IPS_SetName($var, "Licht");
-SetValue($var, false);
-IPS_SetVariableCustomProfile($var, "~Switch");
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
-
-		// Event anlegen
-		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
-		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
-		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
-		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(\$_IPS['VARIABLE']))).\".ips.php\");\n\nLW12_PowerToggle();");
-
- //Modus
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $modulId);
-IPS_SetPosition($var, 20);
-IPS_SetName($var, "Modus");
-SetValue($var, 0);
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+	 //Licht
+	$var = IPS_CreateVariable(0);
+	IPS_SetParent($var, $modulId);
+	IPS_SetPosition($var, 10);
+	IPS_SetName($var, "Licht");
+	SetValue($var, false);
+	IPS_SetVariableCustomProfile($var, "~Switch");
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 
 		// Event anlegen
 		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
 		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
 		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
 		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(\$_IPS['VARIABLE']))).\".ips.php\");\n\nLW12_ModeToggle();");
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_PowerToggle();");
+
+	 //Modus
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $modulId);
+	IPS_SetPosition($var, 20);
+	IPS_SetName($var, "Modus");
+	SetValue($var, 0);
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+
+		// Event anlegen
+		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
+		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
+		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
+		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_ModeToggle();");
 
 		// Var Profil anlegen und zuweisen
       if (!IPS_VariableProfileExists("LW12_LED_MODE")) {
@@ -102,65 +95,65 @@ IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 		IPS_SetVariableCustomProfile($var, "LW12_LED_MODE");
 
 
- //Farbauswahl
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $modulId);
-IPS_SetPosition($var, 30);
-IPS_SetName($var, "Farbauswahl (Rot-Grün-Blau)");
-SetValue($var, 0);
-IPS_SetVariableCustomProfile($var, "~HexColor");
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+	 //Farbauswahl
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $modulId);
+	IPS_SetPosition($var, 30);
+	IPS_SetName($var, "Farbauswahl (Rot-Grün-Blau)");
+	SetValue($var, 0);
+	IPS_SetVariableCustomProfile($var, "~HexColor");
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 
 		// Event anlegen
 		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
 		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
 		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
 		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(\$_IPS['VARIABLE']))).\".ips.php\");\n\nLW12_setDecRGB(GetValue(\$_IPS['VARIABLE']));");
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_setDecRGB(GetValue(\$_IPS['VARIABLE']));");
 
 
 
 
- // Controller-Programme Dummy Instanz anlegen
- // ==========================================
+	 // Controller-Programme Dummy Instanz anlegen
+	 // ==========================================
 
-$subModulId = IPS_CreateInstance("{485D0419-BE97-4548-AA9C-C083EB82E61E}");
+	$subModulId = IPS_CreateInstance("{485D0419-BE97-4548-AA9C-C083EB82E61E}");
 
-IPS_SetParent($subModulId, $modulId);
-IPS_SetName($subModulId, "Controller-Programme");
-IPS_SetPosition($subModulId, 50);
-IPS_SetHidden($subModulId, true);
+	IPS_SetParent($subModulId, $modulId);
+	IPS_SetName($subModulId, "Controller-Programme");
+	IPS_SetPosition($subModulId, 50);
+	IPS_SetHidden($subModulId, true);
 
- //Automatisches Programm
-$var = IPS_CreateVariable(0);
-IPS_SetParent($var, $subModulId);
-IPS_SetPosition($var, 0);
-IPS_SetName($var, "Automatisches Programm");
-SetValue($var, false);
-IPS_SetVariableCustomProfile($var, "~Switch");
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
-
-		// Event anlegen
-		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
-		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
-		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
-		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(IPS_GetParent(\$_IPS['VARIABLE'])))).\".ips.php\");\n\nLW12_CtrlPrgToggle();;");
-
-//Programm
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $subModulId);
-IPS_SetPosition($var, 10);
-IPS_SetName($var, "Programm");
-SetValue($var, 1);
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+	 //Automatisches Programm
+	$var = IPS_CreateVariable(0);
+	IPS_SetParent($var, $subModulId);
+	IPS_SetPosition($var, 0);
+	IPS_SetName($var, "Automatisches Programm");
+	SetValue($var, false);
+	IPS_SetVariableCustomProfile($var, "~Switch");
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 
 		// Event anlegen
 		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
 		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
 		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
 		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(IPS_GetParent(\$_IPS['VARIABLE'])))).\".ips.php\");\n\nLW12_SetCtrlPrg(\$_IPS['VALUE']);");
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_CtrlPrgToggle();;");
+
+	//Programm
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $subModulId);
+	IPS_SetPosition($var, 10);
+	IPS_SetName($var, "Programm");
+	SetValue($var, 1);
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+
+		// Event anlegen
+		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
+		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
+		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
+		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_SetCtrlPrg(\$_IPS['VALUE']);");
 
 		// Var Profil anlegen und zuweisen
       if (!IPS_VariableProfileExists("LW12_CTRL_PRGS")) {
@@ -169,20 +162,20 @@ IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 			}
 		IPS_SetVariableCustomProfile($var, "LW12_CTRL_PRGS");
 
-//Geschwindigkeit
-$var = IPS_CreateVariable(1);
-IPS_SetParent($var, $subModulId);
-IPS_SetPosition($var, 10);
-IPS_SetName($var, "Geschwindigkeit");
-SetValue($var, 1);
-IPS_SetVariableCustomAction($var, $LW12_LibraryId);
+	//Geschwindigkeit
+	$var = IPS_CreateVariable(1);
+	IPS_SetParent($var, $subModulId);
+	IPS_SetPosition($var, 10);
+	IPS_SetName($var, "Geschwindigkeit");
+	SetValue($var, 1);
+	IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 
 		// Event anlegen
 		$eventId = IPS_CreateEvent(0);               //Ausgelöstes Ereignis
 		IPS_SetEventTrigger($eventId, 1, $var);      //Bei Änderung von Variable mit ID $var
 		IPS_SetParent($eventId, $var);         		//Ereignis zuordnen
 		IPS_SetEventActive($eventId, true);          //Ereignis aktivieren
-		IPS_SetEventScript($eventId, "require(\"scripts/\".GetValue(IPS_GetObjectIDByName(\"!LW12_Library\",  IPS_GetParent(IPS_GetParent(\$_IPS['VARIABLE'])))).\".ips.php\");\n\nLW12_SetCtrlPrgSpeed(\$_IPS['VALUE']);");
+		IPS_SetEventScript($eventId, "require(\"scripts/IPSLibrary/app/modules/LedAnsteuerung/LedAnsteuerung_Library.ips.php\");\n\nLW12_SetCtrlPrgSpeed(\$_IPS['VALUE']);");
 
 		// Var Profil anlegen und zuweisen
       if (!IPS_VariableProfileExists("LW12_CTRL_PRGS_SPEED")) {
@@ -191,7 +184,12 @@ IPS_SetVariableCustomAction($var, $LW12_LibraryId);
 			}
 		IPS_SetVariableCustomProfile($var, "LW12_CTRL_PRGS_SPEED");
 
-}
+	}
+$ipadrId = @IPS_GetObjectIDByName("!IP", $modulId);
+
+echo "ModulID:".$modulId."\n";
+echo "IP ID:".$ipadrId."\n";
+SetValue($ipadrId,"10.0.0.50");
 
 
 ?>
