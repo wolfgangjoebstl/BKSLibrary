6a9dc0d7145c25e6cf93af36ba141d297da565d5
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-02-28 15:04:20 +0100

    BKS AS upd

--- IPSLibrary/config/modules/Autosteuerung/Default/Autosteuerung_Configuration.inc.php
+++ IPSLibrary/config/modules/Autosteuerung/Default/Autosteuerung_Configuration.inc.php
@@ -31,6 +31,103 @@
 	 */
 
 
+	/* Beispiele zum EInstellen:
+			21675 => array('OnUpdate','tts_play(1,"Hello","",2);','par2',),
+			21675 => array('OnUpdate','IPSLight_SetSwitchByName("Ventilator", true);','par2',),
+			37416 => array('OnUpdate','IPSLight_SetSwitchByName("Ventilator", false);','par2',),
+			21675 => array('OnUpdate','IPSLight_SetProgramNextByName("WohnzimmerProgram");','par2',),
+			37416 => array('OnUpdate','IPSLight_SetGroupByName("WohnzimmerHue", false);','par2',),
+			24558 => array('OnChange','Anwesenheit','par2',),        für Anwesenheitssimulation, siehe config weiter unten
+	*/
+
+
+
+	function Autosteuerung_GetEventConfiguration() {
+		$eventConfiguration = array(
+			43480 => array('OnChange','IPSLight_SetSwitchByName("Ventilator", true);','par2',),
+			17598 => array('OnChange','IPSLight_SetSwitchByName("Ventilator", false);Sprechen("Hallo Claudia wie gehts ?");','par2',),
+			);
+
+		return $eventConfiguration;
+	}
+
+	/* Beispiele zum Einspielen
+
+	            'GangEG'  =>  array(
+                'NAME'                      => 'GangEG',
+                    'ACTIVE_FROM_TO'        => '20:40-23:00',
+                    'EVENT_CHANCE'        => 10,
+                    'EVENT_DURATION'      => 2,
+                    'EVENT_IPSLIGHT_GRP'  => 'GangEG'
+                    ),
+            'Wohnzimmer' => array(
+                'NAME'                      => 'Wohnzimmer',
+                    'ACTIVE_FROM_TO'        => '20:40-23:00',
+                    'EVENT_CHANCE'        => 20,
+                    'EVENT_DURATION'      => 30,
+                    'EVENT_IPSLIGHT_GRP' => 'Wohnzimmer'
+                    ),
+             'Fernseher' => array(
+                'NAME'                      => 'Fernseher',
+                    'ACTIVE_FROM_TO'        => '19:00-23:00',
+                    'EVENT_CHANCE'        => 10,
+                    'EVENT_DURATION'      => 45,
+                    'EVENT_IPSLIGHT_GRP' => 'Fernseher'
+                    )
+             'AWSTischLampe'  =>  array(
+                	'NAME'                      => 'AWSTischLampe',
+                  'ACTIVE_FROM_TO'        => '20:40-23:00',
+                  'EVENT_CHANCE'        => 20, 	//20% Eintrittswahrscheinlichkeit
+                  'EVENT_DURATION'      => 1,   // Minuten
+                  'EVENT_IPSLIGHT_GRP'  => 'Gaestezimmer'  //zu schaltende IPS-LIGHT Gruppe
+                    ),
+             'AWSWZDeko'  =>  array(
+                	'NAME'                      => 'AWSWZDeko',
+                  'ACTIVE_FROM_TO'        => '20:40-23:00',
+                  'EVENT_CHANCE'        => 20, 	//20% Eintrittswahrscheinlichkeit
+                  'EVENT_DURATION'      => 1,   // Minuten
+                  'EVENT_IPSLIGHT_GRP'  => 'WohnzimmerDeko'  //zu schaltende IPS-LIGHT Gruppe
+                    ),
+             'AWSWZKugel'  =>  array(
+                	'NAME'                      => 'AWSWZKugel',
+                  'ACTIVE_FROM_TO'        => '20:40-23:00',
+                  'EVENT_CHANCE'        => 20, 	//20% Eintrittswahrscheinlichkeit
+                  'EVENT_DURATION'      => 1,   // Minuten
+                  'EVENT_IPSLIGHT_GRP'  => 'WohnzimmerKugel'  //zu schaltende IPS-LIGHT Gruppe
+                    ),
+
+	*/
+
+
+	function Autosteuerung_GetScenes() {
+		 $scenes = array(
+
+
+                );
+		return $scenes;
+	}
+
+
+	function Sprechen($text="Hallo")
+		{
+			$remServer=array(
+				"BKS-Server"           	=> 	'http://wolfgangjoebstl@yahoo.com:cloudg06@10.0.1.6:82/api/',
+						);
+			foreach ($remServer as $Server)
+				{
+				$rpc = new JSONRPC($Server);
+				}
+		$redet=array("Text" => "Claudia");
+		$redet["Text"]=$text;
+		echo $text."\n";
+		//$rpc->IPS_RunScript(10004 /*[Objekt #10004 existiert nicht]*/);
+		$rpc->IPS_RunScriptEx(10004,$redet);
+	   }
+
+
+	 
+	 
+
 
 
 	 
