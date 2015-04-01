01397699d255b311743ff20464377e9f87501827
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2015-04-01 21:23:02 +0200

    LBG_new emailsteuerung

---
+++ IPSLibrary/app/modules/Emailsteuerung/Emailsteuerung.ips.php
@@ -0,0 +1,89 @@
+<?
+
+/***********************************************************************
+
+Sprachsteuerung
+
+***********************************************************/
+
+Include(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
+IPSUtils_Include ("Emailsteuerung_Configuration.inc.php","IPSLibrary::config::modules::Emailsteuerung");
+
+/******************************************************
+
+				INIT
+
+*************************************************************/
+
+$repository = 'https://raw.githubusercontent.com//wolfgangjoebstl/BKSLibrary/master/';
+if (!isset($moduleManager)) {
+	IPSUtils_Include ('IPSModuleManager.class.php', 'IPSLibrary::install::IPSModuleManager');
+
+	echo 'ModuleManager Variable not set --> Create "default" ModuleManager';
+	$moduleManager = new IPSModuleManager('Emailsteuerung',$repository);
+}
+
+$installedModules = $moduleManager->GetInstalledModules();
+$inst_modules="\nInstallierte Module:\n";
+foreach ($installedModules as $name=>$modules)
+	{
+	$inst_modules.=str_pad($name,30)." ".$modules."\n";
+	}
+echo $inst_modules."\n\n";
+
+$CategoryIdData     = $moduleManager->GetModuleCategoryID('data');
+$CategoryIdApp      = $moduleManager->GetModuleCategoryID('app');
+$scriptId  = IPS_GetObjectIDByIdent('Emailsteuerung', IPSUtil_ObjectIDByPath('Program.IPSLibrary.app.modules.Emailsteuerung'));
+echo "Category App ID:".$CategoryIdApp."\n";
+echo "Category Script ID:".$scriptId."\n";
+
+
+
+
+
+
+/*********************************************************************************************/
+
+
+if ($_IPS['SENDER']=="WebFront")
+	{
+	/* vom Webfront aus gestartet */
+
+	}
+
+/*********************************************************************************************/
+
+
+if ($_IPS['SENDER']=="Execute")
+	{
+	/* von der Konsole aus gestartet */
+
+
+	}
+
+/*********************************************************************************************/
+
+
+if ($_IPS['SENDER']=="Variable")
+	{
+
+	}
+
+/*********************************************************************************************/
+
+
+if ($_IPS['SENDER']=="TimerEvent")
+	{
+
+	}
+
+
+/*********************************************************************************************/
+
+
+/*********************************************************************************************/
+
+
+
+
+?>
