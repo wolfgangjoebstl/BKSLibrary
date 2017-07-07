76c84273a3de06439d389de5c865794836f0234d
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2017-07-07 11:34:28 +0200

    LBG SP 047
    works also wo OC

--- IPSLibrary/app/modules/Startpage/Monitor_OnOff.ips.php
+++ IPSLibrary/app/modules/Startpage/Monitor_OnOff.ips.php
@@ -14,10 +14,30 @@
 	 *  Version 2.50.52, 07.08.2014<br/>
 */
 
-IPSUtils_Include ("OperationCenter_Library.class.php","IPSLibrary::app::modules::OperationCenter");
 IPSUtils_Include ('Startpage_Configuration.inc.php', 'IPSLibrary::config::modules::Startpage');
 
-$parentid  = IPSUtil_ObjectIDByPath('Program.IPSLibrary.data.modules.Startpage');
+/******
+ *
+ * Initialisierung
+ *
+ ********/
+ 
+	$repository = 'https://raw.githubusercontent.com//wolfgangjoebstl/BKSLibrary/master/';
+	if (!isset($moduleManager)) 
+		{
+		IPSUtils_Include ('IPSModuleManager.class.php', 'IPSLibrary::install::IPSModuleManager');
+		$moduleManager = new IPSModuleManager('Startpage',$repository);
+		}
+	$installedModules = $moduleManager->GetInstalledModules();
+
+	$parentid  = IPSUtil_ObjectIDByPath('Program.IPSLibrary.data.modules.Startpage');
+
+/******
+ *
+ * Konfiguration
+ *
+ ********/
+
 $configuration=startpage_configuration();
 
 if (isset($_IPS['Monitor']))
@@ -25,12 +45,20 @@ if (isset($_IPS['Monitor']))
    if ($_IPS['Monitor']=="on")
 		{
 		IPS_ExecuteEX($configuration["Directories"]["Scripts"].'nircmd.exe', "sendkeypress F11", false, false, 1);
-		tts_play(1,'Monitor ein','',2);
+		if (isset($installedModules["OperationCenter"])==true)
+			{  /* nur wenn OperationCenter vorhanden auch die lokale Soundausgabe starten*/
+			IPSUtils_Include ("OperationCenter_Library.class.php","IPSLibrary::app::modules::OperationCenter");
+			tts_play(1,'Monitor ein','',2);
+			}
 		}
    if ($_IPS['Monitor']=="off")
 		{
 		IPS_ExecuteEX($configuration["Directories"]["Scripts"].'nircmd.exe', "monitor off", false, false, 1);
-		tts_play(1,'Monitor aus','',2);
+		if (isset($installedModules["OperationCenter"])==true)
+			{  /* nur wenn OperationCenter vorhanden auch die lokale Soundausgabe starten*/
+			IPSUtils_Include ("OperationCenter_Library.class.php","IPSLibrary::app::modules::OperationCenter");
+			tts_play(1,'Monitor aus','',2);
+			}
 		}
    }
 
@@ -39,8 +67,11 @@ if ($_IPS['SENDER']=="Execute")
 	{
 	//IPS_ExecuteEX("c:/Scripts/nircmd.exe", "sendkeypress F11", false, false, 1);
 	//IPS_ExecuteEX("c:/Scripts/nircmd.exe", "monitor off", false, false, 1);
-	tts_play(1,'Monitor aus','',2);
-
+	if (isset($installedModules["OperationCenter"])==true)
+		{  /* nur wenn OperationCenter vorhanden auch die lokale Soundausgabe starten*/
+		IPSUtils_Include ("OperationCenter_Library.class.php","IPSLibrary::app::modules::OperationCenter");
+		tts_play(1,'Ausgabe Monitor Status wird unterstützt','',2);
+		}
 	}
 
 ?>
