cb163b340e9afce01b608822bf57242d36da8f5a
wolfgangjoebstl <wolfgangjoebstl@yahoo.com>
2020-08-30 16:30:21 +0200

    LBG 07 5.4 RA

--- IPSLibrary/app/modules/RemoteAccess/EvaluateAndere.ips.php
+++ IPSLibrary/app/modules/RemoteAccess/EvaluateAndere.ips.php
@@ -17,50 +17,50 @@
 	 * along with the IPSLibrary. If not, see http://www.gnu.org/licenses/gpl.txt.
 	 */ 
 	 
-/* Program baut auf einem remote Server eine Variablenstruktur auf in die dann bei jeder Veränderung Werte geschrieben werden
- *
- * 	hier für den Regensensor und die Temperaturwerte der FHT Heizungssteuerungen/zentralen
- *    Basis ist das File EvaluateVariables_ROID.inc.php
- *
- * RemoteAccess				erzeugt die include Files für EvaluateVariables und erstellt die benötigten Profile
- *
- * EvaluateAndere				Regensensor, FHT Temperatur (TemeratureVar)
- * EvaluateButton				Taster Homematic und FS20EX
- * EvaluateContact			Kontakte Homematic
- * EvaluateHeatControl		Homematic und FHT Thermostate den Stellwert und den Sollwert
- * EvaluateHomematic			Homematic Temperatur und Feuchtigkeitswerte
- * EvaluateMotion				Homematic und FS20 Bewegungsmelder, und die Bewegungsmelder der Cams
- * EvaluateStronverbrauch 	die von AMIS angelegten Register
- * EvaluateSwitch				Homematic, HomematicIP udn FS20 Schalter 
- * EvaluateVariables			Guthaben, SysInfo und RouterDaten Register
- *
- *
- *
- */
+    /* Program baut auf einem remote Server eine Variablenstruktur auf in die dann bei jeder Veränderung Werte geschrieben werden
+    *
+    * 	hier für den Regensensor und die Temperaturwerte der FHT Heizungssteuerungen/zentralen
+    *    Basis ist das File EvaluateVariables_ROID.inc.php
+    *
+    * RemoteAccess				erzeugt die include Files für EvaluateVariables und erstellt die benötigten Profile
+    *
+    * EvaluateAndere				Regensensor, FHT Temperatur (TemeratureVar)
+    * EvaluateButton				Taster Homematic und FS20EX
+    * EvaluateContact			Kontakte Homematic
+    * EvaluateHeatControl		Homematic und FHT Thermostate den Stellwert und den Sollwert
+    * EvaluateHomematic			Homematic Temperatur und Feuchtigkeitswerte
+    * EvaluateMotion				Homematic und FS20 Bewegungsmelder, und die Bewegungsmelder der Cams
+    * EvaluateStronverbrauch 	die von AMIS angelegten Register
+    * EvaluateSwitch				Homematic, HomematicIP udn FS20 Schalter 
+    * EvaluateVariables			Guthaben, SysInfo und RouterDaten Register
+    *
+    *
+    *
+    */
 
-Include(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
-IPSUtils_Include ("RemoteAccess_Configuration.inc.php","IPSLibrary::config::modules::RemoteAccess");
+    Include(IPS_GetKernelDir()."scripts\IPSLibrary\AllgemeineDefinitionen.inc.php");
+    IPSUtils_Include ("RemoteAccess_Configuration.inc.php","IPSLibrary::config::modules::RemoteAccess");
 
-/******************************************************
+    /******************************************************
 
-				INIT
+                    INIT
 
-*************************************************************/
+    *************************************************************/
 
-// max. Scriptlaufzeit definieren
-ini_set('max_execution_time', 500);
-$startexec=microtime(true);
+    // max. Scriptlaufzeit definieren
+    ini_set('max_execution_time', 500);
+    $startexec=microtime(true);
 
-echo "Update Konfiguration und register Events\n";
+    echo "Update Konfiguration und register Events\n";
 
-IPSUtils_Include ("IPSComponentSensor_Temperatur.class.php","IPSLibrary::app::core::IPSComponent::IPSComponentSensor");
-IPSUtils_Include ('IPSMessageHandler.class.php', 'IPSLibrary::app::core::IPSMessageHandler');
-IPSUtils_Include ("EvaluateHardware_Include.inc.php","IPSLibrary::config::modules::EvaluateHardware");
-	
-IPSUtils_Include ("EvaluateVariables_ROID.inc.php","IPSLibrary::app::modules::RemoteAccess");
-$remServer=ROID_List();
+    IPSUtils_Include ("IPSComponentSensor_Temperatur.class.php","IPSLibrary::app::core::IPSComponent::IPSComponentSensor");
+    IPSUtils_Include ('IPSMessageHandler.class.php', 'IPSLibrary::app::core::IPSMessageHandler');
+    IPSUtils_Include ("EvaluateHardware_Include.inc.php","IPSLibrary::config::modules::EvaluateHardware");
+        
+    IPSUtils_Include ("EvaluateVariables_ROID.inc.php","IPSLibrary::app::modules::RemoteAccess");
+    $remServer=ROID_List();
 
-$status=RemoteAccessServerTable();
+    $status=RemoteAccessServerTable();
 
 	$Homematic = HomematicList();
 	//print_r($Homematic);
@@ -69,17 +69,9 @@ $status=RemoteAccessServerTable();
 		/* alle Regensensoren ausgeben */
 		if (isset($Key["COID"]["RAIN_COUNTER"])==true)
 			{
-			echo "Regensensor gefunden:\n";
 			$oid=(integer)$Key["COID"]["RAIN_COUNTER"]["OID"];
-      		$variabletyp=IPS_GetVariable($oid);
-			if ($variabletyp["VariableProfile"]!="")
-			   {
-				echo str_pad($Key["Name"],30)." = ".GetValueFormatted($oid)."   (".date("d.m H:i",IPS_GetVariable($oid)["VariableChanged"]).")       ".number_format((microtime(true)-$startexec),2)." Sekunden\n";
-				}
-			else
-			   {
-				echo str_pad($Key["Name"],30)." = ".GetValue($oid)."   (".date("d.m H:i",IPS_GetVariable($oid)["VariableChanged"]).")       ".number_format((microtime(true)-$startexec),2)." Sekunden\n";
-				}
+			echo "Regensensor gefunden  $oid ".IPS_GetName($oid)."/".IPS_GetName(IPS_GetParent($oid))."/".IPS_GetName(IPS_GetParent(IPS_GetParent($oid))).":\n";
+    		echo str_pad($oid,8).str_pad($Key["Name"],30)." = ".GetValueIfFormatted($oid)."   (".date("d.m H:i",IPS_GetVariable($oid)["VariableChanged"]).")       ".number_format((microtime(true)-$startexec),2)." Sekunden\n";
 			$parameter="";
 			foreach ($remServer as $Name => $Server)
 				{
@@ -95,60 +87,14 @@ $status=RemoteAccessServerTable();
 					$parameter.=$Name.":".$result.";";
 					}
 				}
-		   $messageHandler = new IPSMessageHandler();
-		   $messageHandler->CreateEvents(); /* * Erzeugt anhand der Konfiguration alle Events */
-		   $messageHandler->CreateEvent($oid,"OnChange");  /* reicht nicht aus, wird für HandleEvent nicht angelegt */
-			$messageHandler->RegisterEvent($oid,"OnChange",'IPSComponentSensor_Counter,'.$parameter,'IPSModuleSensor_Counter');
-			echo "Regenfall Register mit Parameter :".$parameter." erzeugt.\n";
-		   //print_r($Key);
-		   }
-		}
-
-if (false)
-    {
-	$FHT = FHTList();
-	$keyword="TemeratureVar";
-
-	foreach ($FHT as $Key)
-		{
-		/* alle Temperaturwerte der Heizungssteuerungen ausgeben */
-		if (isset($Key["COID"][$keyword])==true)
-  			{
-		    echo "FHT Heizungssteuerung gefunden:\n";
-	        $oid=(integer)$Key["COID"][$keyword]["OID"];
-      	    $variabletyp=IPS_GetVariable($oid);
-			if ($variabletyp["VariableProfile"]!="")
-			   {
-				echo str_pad($Key["Name"],30)." = ".GetValueFormatted($oid)."   (".date("d.m H:i",IPS_GetVariable($oid)["VariableChanged"]).")       ".number_format((microtime(true)-$startexec),2)." Sekunden\n";
-				}
-			else
-	   		    {
-				echo str_pad($Key["Name"],30)." = ".GetValue($oid)."   (".date("d.m H:i",IPS_GetVariable($oid)["VariableChanged"]).")       ".number_format((microtime(true)-$startexec),2)." Sekunden\n";
-				}
-			//$DetectTemperatureHandler->RegisterEvent($oid,"Temperatur",'','par3');
-			
-			$parameter="";
-			foreach ($remServer as $Name => $Server)
-				{
-				echo "   Server : ".$Name." mit Adresse ".$Server["Adresse"]."  Erreichbar : ".($status[$Name]["Status"] ? 'Ja' : 'Nein')."\n";
-				if ( $status[$Name]["Status"] == true )
-					{				
-					$rpc = new JSONRPC($Server["Adresse"]);
-					$result=RPC_CreateVariableByName($rpc, (integer)$Server["Temperatur"], $Key["Name"], 2);
-					$rpc->IPS_SetVariableCustomProfile($result,"Temperatur");
-					$rpc->AC_SetLoggingStatus((integer)$Server["ArchiveHandler"],$result,true);
-					$rpc->AC_SetAggregationType((integer)$Server["ArchiveHandler"],$result,0); /* 0 Standard 1 ist Zähler */
-					$rpc->IPS_ApplyChanges((integer)$Server["ArchiveHandler"]);				//print_r($result);
-					$parameter.=$Name.":".$result.";";
-					}
-				}
 		    $messageHandler = new IPSMessageHandler();
 		    $messageHandler->CreateEvents(); /* * Erzeugt anhand der Konfiguration alle Events */
 		    $messageHandler->CreateEvent($oid,"OnChange");  /* reicht nicht aus, wird für HandleEvent nicht angelegt */
-			$messageHandler->RegisterEvent($oid,"OnChange",'IPSComponentSensor_Temperatur,'.$parameter,'IPSModuleSensor_Temperatur,1,2,3');
-			echo "Heizungssteuerungs Register mit Parameter :".$parameter." erzeugt.\n";
-			}
+			$messageHandler->RegisterEvent($oid,"OnChange",'IPSComponentSensor_Counter,'.$parameter,'IPSModuleSensor_Counter');
+			echo "Regenfall Register mit Parameter :".$parameter." erzeugt.\n";
+		    //print_r($Key);
+		    }
 		}
-    }
+
 
 ?>
